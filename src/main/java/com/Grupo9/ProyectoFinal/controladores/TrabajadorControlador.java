b584c47e06357d02129a8a5aadf3a3abc58a6088
Andres Ramirez <andresnolberto@gmail.com>
2021-11-09 20:24:46 -0300

    pull resuelto

--- src/main/java/com/Grupo9/ProyectoFinal/controladores/TrabajadorControlador.java
+++ src/main/java/com/Grupo9/ProyectoFinal/controladores/TrabajadorControlador.java
@@ -1,72 +1,85 @@
 package com.Grupo9.ProyectoFinal.controladores;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
+import java.io.IOException;
 import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.Grupo9.ProyectoFinal.Entidad.Empleo;
 import com.Grupo9.ProyectoFinal.Entidad.Trabajador;
 import com.Grupo9.ProyectoFinal.Enum.Genero;
 import com.Grupo9.ProyectoFinal.Enum.Oficio;
-import com.Grupo9.ProyectoFinal.Enum.Tipo;
 import com.Grupo9.ProyectoFinal.Enum.Zona;
 import com.Grupo9.ProyectoFinal.Servicios.EmpleoServicio;
 import com.Grupo9.ProyectoFinal.Servicios.TrabajadorServicio;
-import java.io.IOException;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/trabajador")
 public class TrabajadorControlador {
-	
+
 	@Autowired
 	private TrabajadorServicio trabajadorServicio;
-	
+
 	@Autowired
 	private EmpleoServicio empleoServicio;
-	
+
 	@GetMapping("/")
 	public String index(ModelMap model) {
-       List<Empleo> listaEmpleos = empleoServicio.listarEmpleos();
-      
-        model.addAttribute("listaEmpleos", listaEmpleos);
-		
+		List<Empleo> listaEmpleos = empleoServicio.listarEmpleos();
+
+		model.addAttribute("listaEmpleos", listaEmpleos);
+
 		return "index_trabajadores";
 	}
-	
-	
+
 	@GetMapping("/registro-trabajador")
 	public String registroEmpleador() {
 		return "registro-trabajador";
 	}
-	
+
 	@PostMapping("/registro-trabajador")
-	public String registroRecibido(@RequestParam("email") String email, @RequestParam("contrasena") String contrasena,@RequestParam("contrasena2") String contrasena2,@RequestParam("nombre") String nombre, @RequestParam("apellido") String apellido,
-			@RequestParam("genero") Genero genero, @RequestParam("fechaNacimiento") Date fechaNacimiento, @RequestParam("zona") Zona zona, @RequestParam("telefono") String telefono,
-			@RequestParam("oficio") Oficio oficio, @RequestParam("experiencia") String experiencia, @RequestParam("disponible") Boolean disponible, @RequestParam("licencia") Boolean licencia, @RequestParam("skills") String skills){
+	public String registroRecibido(ModelMap model, @RequestParam("email") String email,
+			@RequestParam("contrasena") String contrasena, @RequestParam("contrasena2") String contrasena2,
+			@RequestParam("nombre") String nombre, @RequestParam("apellido") String apellido,
+			@RequestParam("genero") Genero genero, @RequestParam("fechaNacimiento") Date fechaNacimiento,
+			@RequestParam("zona") Zona zona, @RequestParam("telefono") String telefono,
+			@RequestParam("oficio") Oficio oficio, @RequestParam("experiencia") String experiencia,
+			@RequestParam(defaultValue = "no") Boolean disponible, @RequestParam(defaultValue = "no") Boolean licencia,
+			@RequestParam("skills") String skills) {
 		try {
-			
-			trabajadorServicio.crearTrabajador(email,contrasena,contrasena2,nombre,apellido,genero,fechaNacimiento,zona,telefono,oficio,experiencia,disponible,licencia,skills);
+			trabajadorServicio.crearTrabajador(email, contrasena, contrasena2, nombre, apellido, genero,
+					fechaNacimiento, zona, telefono, oficio, experiencia, disponible, licencia, skills);
 		} catch (Exception e) {
-			System.out.print(e);
+			model.put("email", email);
+			model.put("nombre", nombre);
+			model.put("apellido", apellido);
+			model.put("genero", genero);
+			model.put("fechaNacimiento", fechaNacimiento);
+			model.put("zona", zona);
+			model.put("telefono", telefono);
+			model.put("oficio", Oficio.valueOf(oficio));
+			model.put("experiencia", experiencia);
+			model.put("disponible", disponible);
+			model.put("skills", skills);
+			model.put("error", e.getMessage());
+			return "registro-trabajador";
 		}
-		
+
 		return "redirect:/";
-	
+
 	}
-	
-	
+
 	@GetMapping("/perfil/{id}")
 	public String perfilTrabajador(ModelMap model, @PathVariable("id") Long id) {
 		Trabajador trabajador = trabajadorServicio.encontrarPorId(id);
@@ -92,11 +105,5 @@ public class TrabajadorControlador {
             trabajadorServicio.modificarTrabajador(id, nombre, apellido, genero, fechaNacimiento, zona, telefono, oficio, experiencia, disponible, licencia, skills, imagen, experiencia);
          return  "perfil_trabajadorr";   
         }
-      
-	
-          
-	
-	
-	
 
 }
