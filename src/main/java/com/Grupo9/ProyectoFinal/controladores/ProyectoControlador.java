85bad228041cc28d6b6174bdb7e8431492bd9347
Andres Ramirez <andresnolberto@gmail.com>
2021-11-17 18:27:57 -0300

    Error del login

--- src/main/java/com/Grupo9/ProyectoFinal/controladores/ProyectoControlador.java
+++ src/main/java/com/Grupo9/ProyectoFinal/controladores/ProyectoControlador.java
@@ -1,9 +1,11 @@
 package com.Grupo9.ProyectoFinal.controladores;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -13,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.Grupo9.ProyectoFinal.Entidad.Empleador;
-import com.Grupo9.ProyectoFinal.Entidad.Trabajador;
-import com.Grupo9.ProyectoFinal.Enum.Oficio;
 import com.Grupo9.ProyectoFinal.Servicios.EmpleadorServicio;
 import com.Grupo9.ProyectoFinal.Servicios.EmpleoServicio;
 import com.Grupo9.ProyectoFinal.Servicios.TrabajadorServicio;
@@ -39,6 +39,21 @@ public class ProyectoControlador {
 
 	@GetMapping("/login")
 	public String login() {
+		return "login.html";
+	}
+	
+	@GetMapping("/login-error")
+	public String login(HttpServletRequest request, Model model) {
+		HttpSession session = request.getSession(false);
+        String errorMessage = null;
+        if (session != null) {
+            AuthenticationException ex = (AuthenticationException) session
+                    .getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+            if (ex != null) {
+                errorMessage = "Email o contraseña incorrecto";
+            }
+        }
+        model.addAttribute("error", errorMessage);
 		return "login.html";
 	}
 
@@ -87,7 +102,7 @@ public class ProyectoControlador {
 	
 	@GetMapping("/como-funciona")
 	public String comoFunciona() {
-		return "comoFunciona";
+		return "informacion";
 	}
 
 
